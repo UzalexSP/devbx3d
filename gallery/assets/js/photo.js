6be0eb2d89330e1e6ca312aabd166effc5fc3085
DevBX3D <zirenkogd@gmail.com>
2026-09-04 14:14:43 +0300

    обратно замена png на jpg на одном из файлов

--- gallery/assets/js/photo.js
+++ gallery/assets/js/photo.js
@@ -133,7 +133,7 @@ const photos = [
                 desc: "Первая RAW фотография, очень ею рад.",
                 device: "iPhone 15 Pro",
                 preview: "/assets/images/p_18.webp",
-                original: "/assets/images/nform/Вечерний мост.png"
+                original: "/assets/images/nform/Вечерний мост.jpg"
             },
             {
                 id: "19",
